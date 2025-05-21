2c4ae2e60bd8e473857bba08ca6d67661114f1fe
Felipedbbsb <felipedbbsb@gmail.com>
2025-05-21 11:29:44 -0300

    Print user editor line text on terminal

--- src/editor.js
+++ src/editor.js
@@ -1,12 +1,23 @@
+// src/editor.js
 import { basicSetup } from 'codemirror'
 import { EditorView } from '@codemirror/view'
 
 function initializeEditor() {
-    const view = new EditorView({
-        doc: "Start document",
-        parent: document.querySelector('#editor'),
-        extensions: [basicSetup]
-    })
+  const view = new EditorView({
+    doc: "Start document",
+    parent: document.querySelector('#editor'), 
+    extensions: [
+      basicSetup,
+      EditorView.updateListener.of(update => {
+        if (update.docChanged) {
+          // get the full text as a string
+          const code = view.state.doc.toString()
+          // print it to the console
+          console.log(code)
+        }
+      })
+    ]
+  })
 }
 
 export { initializeEditor }
