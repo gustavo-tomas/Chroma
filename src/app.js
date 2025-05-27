4865625bb75b061bc9d2d5c8e3d005fb0d8e3c4e
Felipedbbsb <felipedbbsb@gmail.com>
2025-05-27 17:59:16 -0300

    Organized main with only bootstraper, created app and resizer

---
+++ src/app.js
@@ -0,0 +1,38 @@
+import "./style.css";
+import { Graphics } from "./graphics.js";
+import { Editor } from "./editor.js";
+import { setupResizers } from "./resizer.js";
+
+const vertexCode = `varying vec3 vUv;
+void main() {
+    vUv = position;
+    vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
+    gl_Position = projectionMatrix * modelViewPosition;
+}`;
+
+const fragmentCode = `uniform vec3 colorA;
+uniform vec3 colorB;
+varying vec3 vUv;
+void main() {
+    gl_FragColor = vec4(mix(colorA, colorB, vUv.z), 1.0);
+}`;
+
+export class App {
+  init() {
+    this.graphics = new Graphics(vertexCode, fragmentCode);
+    this.editor = new Editor(
+      vertexCode,
+      fragmentCode,
+      this.onEditorUpdate.bind(this)
+    );
+    setupResizers(this.graphics);
+  }
+
+  onEditorUpdate(tab, code) {
+    if (tab === "vertex") {
+      this.graphics.onVertexCodeUpdate(code);
+    } else {
+      this.graphics.onFragmentCodeUpdate(code);
+    }
+  }
+}
